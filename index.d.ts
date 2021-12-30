46f9bf260c19f99c2824285d699cf34ccb491c36
Remco Haszing <remcohaszing@gmail.com>
2021-12-30 16:08:10 +0100

    Fix `VFileValue` `Buffer` type
    Closes GH-64.
    
    Reviewed-by: Christian Murphy <christian.murphy.42@gmail.com>
    Reviewed-by: Titus Wormer <tituswormer@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,24 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment, @typescript-eslint/prefer-ts-expect-error
+// @ts-ignore It’s important to preserve this ignore statement. This makes sure
+// it works both with and without node types.
+import {Buffer} from 'buffer'
+
+/**
+ * This is the same as `Buffer` if node types are included, `never` otherwise.
+ */
+type MaybeBuffer = any extends Buffer ? never : Buffer
+
+/**
+ * Contents of the file.
+ * Can either be text, or a Buffer like structure.
+ * This does not directly use type `Buffer`, because it can also be used in a
+ * browser context.
+ * Instead this leverages `Uint8Array` which is the base type for `Buffer`,
+ * and a native JavaScript construct.
+ */
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export type VFileValue = string | MaybeBuffer
+
 /**
  * This map registers the type of the `data` key of a `VFile`.
  *
@@ -24,7 +45,6 @@ export type VFileData = Record<string, unknown> & Partial<VFileDataMap>
 
 export type {
   BufferEncoding,
-  VFileValue,
   VFileOptions,
   VFileCompatible,
   VFileReporterSettings,
