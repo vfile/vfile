b7ace0ef22730715aab59723e378f3e0018ea0b0
Pablo Henrique Silva <pablohpsilva@gmail.com>
2022-06-18 00:04:40 +0200

    chore: revert previous changes

--- index.ts
+++ index.ts
@@ -1,15 +1,1 @@
 export * from './lib/index'
-
-export type {
-  BufferEncoding,
-  Map,
-  Compatible,
-  Options,
-  Reporter,
-  ReporterSettings,
-  Compatible as VFileCompatible,
-  Options as VFileOptions,
-  Reporter as VFileReporter,
-  ReporterSettings as VFileReporterSettings
-} from './lib/index'
-
