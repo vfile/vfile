983faaa4e8a782b1b545fa0a759435692d25e422
Pablo Henrique Silva <pablohpsilva@gmail.com>
2022-06-18 00:04:08 +0200

    chore: exported a few types

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
 export * from './lib/index'
+
+export type {
+  BufferEncoding,
+  Map,
+  Compatible,
+  Options,
+  Reporter,
+  ReporterSettings,
+  Compatible as VFileCompatible,
+  Options as VFileOptions,
+  Reporter as VFileReporter,
+  ReporterSettings as VFileReporterSettings
+} from './lib/index'
+
